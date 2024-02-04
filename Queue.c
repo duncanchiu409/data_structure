de432cc3493289c127321e99ff1ab927ad594aaa
duncanchiu094 <chiuduncan409@gmail.com>
2024-02-05 10:07:04 +1030

    [add] Queue

--- Queue.c
+++ Queue.c
@@ -8,11 +8,10 @@ typedef struct Queue{
     int rear;
 } Queue;
 
-Queue* root;
-
-void initialQueue(Queue* q){
-    q->front = -1;
-    q->rear = -1;
+void initialQueue(Queue** q){
+    *q = malloc(sizeof(Queue));
+    (*q)->front = -1;
+    (*q)->rear = -1;
 }
 
 int isFull(Queue* q){
@@ -36,10 +35,10 @@ int isEmpty(Queue* q){
 void enqueue(Queue* q, int a){
     if(isFull(q)){
         printf("Queue overflow\n");
-        return;
     }
     else if(isEmpty(q)){
         q->front = q->rear = 0;
+        q->a[q->rear] = a;
     }
     else{
         q->rear = (q->rear + 1) % MAX;
@@ -48,16 +47,35 @@ void enqueue(Queue* q, int a){
 }
 
 void dequeue(Queue* q, int a){
-    if(){
-        
-    }
+    
 }
 
 void display(Queue* q){
-    if(isEmpty(q)){
+    if(isEmpty(q) == 1){
         printf("Empty Queue\n");
     }
     else{
-        
+        printf("Front: %d\n", q->a[q->front]);
+        printf("Rear: %d\n", q->a[q->rear]);
+
+        printf("Queue: ");
+        int i = q->front;
+        while(i != q->rear){
+            printf("%d ", q->a[i]);
+            i = i + 1 == MAX ? 0 : i + 1;
+        }
+        printf("%d\n", q->a[q->rear]);
     }
+}
+
+int main(){
+    Queue** root;
+    initialQueue(root);
+
+    //enqueue(root, 1);
+    //enqueue(root, 2);
+
+    display(*root);
+
+    return 1;
 }
